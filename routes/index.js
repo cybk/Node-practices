e865e6a99dc318543b39056d7d8a242a03c72871
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-10-29 21:28:56 -0600

    starting with some practices for routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
+  const wes = {name: 'Wes', age: 100, cool: true};
   res.send('Hey! It works!');
+  //res.json(wes);
+});
+
+router.get('/reverse/:name',  (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  console.log([...req.params.name].reverse());
+  res.send(reverse);
 });
 
 module.exports = router;
