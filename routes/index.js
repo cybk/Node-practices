9f3202b17bb2fc860b8ab3b88ad5e03c21ecc954
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-11-28 21:01:25 -0600

    small practice with controllers

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  const wes = {name: 'Wes', age: 100, cool: true};
-  //res.send('Hey! It works!');
-  //res.json(wes);
-  //res.json(req.query);
-  res.render('hello', {
-    name: 'Wes',
-    dog: req.query.dog || 'Snickers',
-    title: 'I love food'
-  } );
-});
-
-router.get('/reverse/:name',  (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  console.log([...req.params.name].reverse());
-  res.send(reverse);
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
