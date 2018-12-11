82c7781e9f20ba4afa9130ae7ae1b099a70fb328
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2018-12-10 21:21:15 -0600

    Added the gravatar and security for some routes

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 router.get('/', catchErrors(storeController.getStores));
 // Routing for stores
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
@@ -26,6 +26,7 @@ router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 //routing for users
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 // 1 validate registration data
@@ -35,5 +36,7 @@ router.post('/register',
         userController.validateRegister,
         catchErrors(userController.register),
         authController.login);
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
