2256d7d0ebd184d74432f36a197ff6c6a7edd565
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2019-06-18 21:29:46 -0500

    Updated the store in order to add the favorite functionlity

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -4,10 +4,13 @@ import { $, $$ } from './modules/bling';
 import autocomplete from './modules/autocomplete';
 import typeAhead from './modules/typeAhead';
 import makeMap from './modules/map';
-import { type } from 'os';
+import ajaxHeart from './modules/heart';
 
 autocomplete($('#address'), $('#lat'), $('#lng'));
 
 typeAhead($('.search'));
 
 makeMap($('#map'));
+
+const heartForms = $$('form.heart');
+heartForms.on('submit', ajaxHeart);
