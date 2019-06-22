6f43267aa81151c0d57be2024260e0aa25f6fb2a
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2019-06-21 21:05:02 -0500

    Adding the reviews system for stores

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -918,8 +918,7 @@ module.exports = function buildURL(url, params, paramsSerializer) {
 };
 
 /***/ }),
-/* 11 */,
-/* 12 */
+/* 11 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -949,6 +948,43 @@ function autocomplete(input, latInput, lngInput) {
 }
 
 exports.default = autocomplete;
+
+/***/ }),
+/* 12 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _axios = __webpack_require__(2);
+
+var _axios2 = _interopRequireDefault(_axios);
+
+var _bling = __webpack_require__(1);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function ajaxHeart(e) {
+    var _this = this;
+
+    e.preventDefault();
+    _axios2.default.post(this.action).then(function (res) {
+        var isHearted = _this.heart.classList.toggle('heart__button--hearted');
+        (0, _bling.$)('.heart-count').innerText = res.data.hearts.length;
+        if (isHearted) {
+            _this.heart.classList.add('heart__button--float');
+            setTimeout(function () {
+                _this.heart.classList.remove('heart__button--float');
+            }, 2500);
+        }
+    }).catch(console.error);
+}
+
+exports.default = ajaxHeart;
 
 /***/ }),
 /* 13 */
@@ -2890,7 +2926,7 @@ __webpack_require__(15);
 
 var _bling = __webpack_require__(1);
 
-var _autocomplete = __webpack_require__(12);
+var _autocomplete = __webpack_require__(11);
 
 var _autocomplete2 = _interopRequireDefault(_autocomplete);
 
@@ -2902,7 +2938,7 @@ var _map = __webpack_require__(13);
 
 var _map2 = _interopRequireDefault(_map);
 
-var _heart = __webpack_require__(36);
+var _heart = __webpack_require__(12);
 
 var _heart2 = _interopRequireDefault(_heart);
 
@@ -2916,44 +2952,6 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var heartForms = (0, _bling.$$)('form.heart');
 heartForms.on('submit', _heart2.default);
-
-/***/ }),
-/* 35 */,
-/* 36 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-var _axios = __webpack_require__(2);
-
-var _axios2 = _interopRequireDefault(_axios);
-
-var _bling = __webpack_require__(1);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function ajaxHeart(e) {
-    var _this = this;
-
-    e.preventDefault();
-    _axios2.default.post(this.action).then(function (res) {
-        var isHearted = _this.heart.classList.toggle('heart__button--hearted');
-        (0, _bling.$)('.heart-count').innerText = res.data.hearts.length;
-        if (isHearted) {
-            _this.heart.classList.add('heart__button--float');
-            setTimeout(function () {
-                _this.heart.classList.remove('heart__button--float');
-            }, 2500);
-        }
-    }).catch(console.error);
-}
-
-exports.default = ajaxHeart;
 
 /***/ })
 /******/ ]);
