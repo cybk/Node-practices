597013a93570e2366ac3b25555f75d69f8117b5b
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2019-06-26 21:53:04 -0500

    Added the top scorecards page

--- routes/index.js
+++ routes/index.js
@@ -52,6 +52,8 @@ router.get('/map/', catchErrors(storeController.mapPage));
 router.get('/hearts', catchErrors(storeController.getHearts));
 router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
 
+router.get('/top', catchErrors(storeController.getTopStores));
+
 ///////////////////////////////////////
 //////////// API /////////////////////
 //////////////////////////////////////
